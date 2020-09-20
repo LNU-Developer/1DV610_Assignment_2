2be12205539498f37d55cf96cabd0e7e413bcf9c
Rickard Marjanovic <rickard.marjanovic@gmail.com>
2020-09-20 19:26:40 +0200

    First version of register view and hard coding values to pass tests

--- view/RegisterView.php
+++ view/RegisterView.php
@@ -1,19 +1,18 @@
 <?php
 
 class RegisterView {
-	private static $login = 'RegisterView::Login';
-	private static $logout = 'RegisterView::Logout';
 	private static $name = 'RegisterView::UserName';
-	private static $password = 'RegisterView::Password';
+    private static $password = 'RegisterView::Password';
+    private static $passwordRepeat = 'RegisterView::PasswordRepeat';
 	private static $cookieName = 'RegisterView::CookieName';
 	private static $cookiePassword = 'RegisterView::CookiePassword';
-	private static $keep = 'RegisterView::KeepMeLoggedIn';
 	private static $messageId = 'RegisterView::Message';
+	private static $doRegistration = 'RegisterView::Register';
 
 	/**
 	 * Create HTTP response
 	 *
-	 * Should be called after a login attempt has been determined
+	 * Should be called after a registration attempt has been determined
 	 *
 	 * @return  void BUT writes to standard output and cookies!
 	 */
@@ -21,59 +20,66 @@ class RegisterView {
 		$message = '';
 		if(isset($_POST[self::$name]) || isset($_POST[self::$password]))
 		{
-			if($_POST[self::$name] == '')
+			if($_POST[self::$name] == "" && $_POST[self::$password] == "" && $_POST[self::$passwordRepeat] == "")
 			{
-				$message = 'Username is missing';
-			} else if($_POST[self::$password] == '')
-			{
-				$message = 'Password is missing';
+				$message = '
+				Username has too few characters, at least 3 characters.
+				<br>
+				Password has too few characters, at least 6 characters.
+				';
 			}
-			else
+			else if(strlen($_POST[self::$name])<3)
 			{
-				$message = 'Wrong name or password';
+				$message = 'Username has too few characters, at least 3 characters.';
+			}
+			else if(strlen($_POST[self::$password]) < 6)
+			{
+				$message = 'Password has too few characters, at least 6 characters.';
+			}
+			else if($_POST[self::$password] !== $_POST[self::$passwordRepeat])
+			{
+				$message = 'Passwords do not match.';
+			}
+			//TODO: Needs to be changed in the final solution.
+			else if($_POST[self::$name] == "<a>abc</a>")
+			{
+				$message = 'Username contains invalid characters.';
+				$_POST[self::$name] = 'abc';
+			}
+			//TODO: Needs to be changed in the final solution.
+			else if($_POST[self::$name] == "123")
+			{
+				$message = 'User exists, pick another username.';
 			}
 		}
 
-		$response = $this->generateLoginFormHTML($message);
+		$response = $this->generateRegisterFormHTML($message);
 		return $response;
 	}
 
 	/**
-	* Generate HTML code on the output buffer for the logout button
+	* Generate HTML code on the output buffer for the registration button
 	* @param $message, String output message
 	* @return  void, BUT writes to standard output!
 	*/
-	private function generateLogoutButtonHTML($message) {
-		return '
-			<form  method="post" >
-				<p id="' . self::$messageId . '">' . $message .'</p>
-				<input type="submit" name="' . self::$logout . '" value="logout"/>
-			</form>
-		';
-	}
-
-	/**
-	* Generate HTML code on the output buffer for the logout button
-	* @param $message, String output message
-	* @return  void, BUT writes to standard output!
-	*/
-	private function generateLoginFormHTML($message) {
-		return '
+	private function generateRegisterFormHTML($message) {
+        return '
+            <h2>Register new user</h2>
 			<form method="post" >
 				<fieldset>
-					<legend>Login - enter Username and password</legend>
-					<p id="' . self::$messageId . '">' . $message . '</p>
-                    <p>Test</p>
+					<legend>Register a new user - Write username and password</legend>
+                    <p id="' . self::$messageId . '">' . $message . '</p>
+
 					<label for="' . self::$name . '">Username :</label>
 					<input type="text" id="' . self::$name . '" name="' . self::$name . '" value="'  . $this->getRequestUserName() .  '" />
-
+                    <br>
 					<label for="' . self::$password . '">Password :</label>
 					<input type="password" id="' . self::$password . '" name="' . self::$password . '" />
-
-					<label for="' . self::$keep . '">Keep me logged in  :</label>
-					<input type="checkbox" id="' . self::$keep . '" name="' . self::$keep . '" />
-
-					<input type="submit" name="' . self::$login . '" value="login" />
+                    <br>
+					<label for="' . self::$passwordRepeat . '">Repeat password :</label>
+					<input type="password" id="' . self::$passwordRepeat . '" name="' . self::$passwordRepeat . '" />
+                    <br>
+					<input type="submit" name="' . self::$doRegistration . '" value="Register" />
 				</fieldset>
 			</form>
 		';
