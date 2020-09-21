ba62a2d5536563a4fe39db598e9dedaa7f70835a
Rickard Marjanovic <rickard.marjanovic@gmail.com>
2020-09-21 23:47:20 +0200

    Adding mySQL database with persistant storage of registered users

--- view/LoginView.php
+++ view/LoginView.php
@@ -30,7 +30,34 @@ class LoginView {
 			}
 			else
 			{
-				$message = 'Wrong name or password';
+				$db = mysqli_connect('localhost', 'root', '', 'assignment2');
+				$username = $_POST[self::$name];
+				$password = $_POST[self::$password];
+				//TODO: real_escape_string
+				$password = md5($password);
+				$sql = "SELECT id FROM users WHERE username=? AND password=?";
+				$stmt = mysqli_stmt_init($db);
+				if(!mysqli_stmt_prepare($stmt, $sql))
+				{
+					$message = 'Failed';
+				}
+				else
+				{
+					mysqli_stmt_bind_param($stmt, "ss", $username, $password);
+					mysqli_stmt_execute($stmt);
+					mysqli_stmt_store_result($stmt);
+
+					if(mysqli_stmt_num_rows($stmt)>0)
+					{
+						$message = 'SUCCESS';
+					}
+					else
+					{
+						$message = 'Wrong name or password';
+					}
+				}
+				mysqli_stmt_close($stmt);
+				mysqli_close($db);
 			}
 		}
 
--- view/RegisterView.php
+++ view/RegisterView.php
@@ -20,7 +20,7 @@ class RegisterView {
 		$message = '';
 		if(isset($_POST[self::$name]) || isset($_POST[self::$password]))
 		{
-			if($_POST[self::$name] == "" && $_POST[self::$password] == "" && $_POST[self::$passwordRepeat] == "")
+			if($_POST[self::$name] == "" && $_POST[self::$password] == "")
 			{
 				$message = '
 				Username has too few characters, at least 3 characters.
@@ -46,10 +46,48 @@ class RegisterView {
 				$message = 'Username contains invalid characters.';
 				$_POST[self::$name] = 'abc';
 			}
-			//TODO: Needs to be changed in the final solution.
-			else if($_POST[self::$name] == "123")
-			{
-				$message = 'User exists, pick another username.';
+			else {
+				$db = mysqli_connect('localhost', 'root', '', 'assignment2');
+				//TODO: real_escape_string
+
+				$sql = "SELECT id FROM users WHERE username=?";
+				$stmt = mysqli_stmt_init($db);
+				if(!mysqli_stmt_prepare($stmt, $sql))
+				{
+					$message = 'Failed';
+				}
+				else
+				{
+					$username = $_POST[self::$name];
+					mysqli_stmt_bind_param($stmt, "s", $username);
+					mysqli_stmt_execute($stmt);
+					mysqli_stmt_store_result($stmt);
+
+					if(mysqli_stmt_num_rows($stmt)>0)
+					{
+						$message = "User exists, pick another username.";
+					}
+					else
+					{
+						$sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+						$stmt = mysqli_stmt_init($db);
+						if(!mysqli_stmt_prepare($stmt, $sql))
+						{
+							$message = 'Failed';
+						}
+						else
+						{
+							$password = $_POST[self::$password];
+							$password = md5($password);
+							mysqli_stmt_bind_param($stmt, "ss", $username, $password);
+							mysqli_stmt_execute($stmt);
+							mysqli_stmt_store_result($stmt);
+							$message = 'REGISTERED';
+
+						}
+					}
+					mysqli_stmt_close($stmt);
+					mysqli_close($db);}
 			}
 		}
 
